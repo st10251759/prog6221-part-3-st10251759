4dcdbd07d64b8725d6c3bb17de98903594b472d2
st10251759 <ST10251759@vcconnect.edu.za>
2024-06-06 15:08:49 +0200

    Imporved Recipe and Ingreient Class to work with wpf

--- Ingredient.cs
+++ Ingredient.cs
@@ -9,66 +9,66 @@ namespace ST10251759_PROG6221_POE_P3
 {//namespace begin
     public class Ingredient
     {//Ingredient Class Begin
-        //attribute variable declaration - with getter and setter methods 
-        public string Name { get; set; }
-        public double Quantity { get; set; }
-        //public string Unit { get; set; }
-        public double OriginalQuantity { get; set; } //variable to hold orginal value for Quanity used in reset method
-        public UnitOfMeasurement OriginalUnit { get; set; } //variable to hold orginal Unit of measurement used in reset method
-        public UnitOfMeasurement Unit { get; set; } //enum for unit of measurement
-
-        public FoodGroup FoodGroup { get; set; } //enum for food group
-        public double calories { get; set; } //variable to hold the  calories for each ingredient
-        public double originalCalories { get; set; }//variable to hold the the orginal quanity used when calling the reset recipe method
-
-        //contructor method without parameters
-        public Ingredient()
+        List<UnitOfMeasurement> units = new List<UnitOfMeasurement>();
+        List<FoodGroup> groups = new List<FoodGroup>();
+        public void populateLists()
         {
+            units.Add(UnitOfMeasurement.SMALL);
+            units.Add(UnitOfMeasurement.MEDIUM);
+            units.Add(UnitOfMeasurement.LARGE);
+            units.Add(UnitOfMeasurement.EXTRALARGE);
+            units.Add(UnitOfMeasurement.TEASPOON);
+            units.Add(UnitOfMeasurement.TEASPOONS);
+            units.Add(UnitOfMeasurement.TABLESPOON);
+            units.Add(UnitOfMeasurement.TABLESPOONS);
+            units.Add(UnitOfMeasurement.CUP);
+            units.Add(UnitOfMeasurement.CUPS);
+
+
+            groups.Add(FoodGroup.CARBOHYDRATE);
+            groups.Add(FoodGroup.PROTEIN);
+            groups.Add(FoodGroup.FAT);
+            groups.Add(FoodGroup.FRUIT);
+            groups.Add(FoodGroup.VEGETABLE);
+            groups.Add(FoodGroup.DAIRY);
+
+        }// 
+
+        private string ingredientName;
+        private double quantity;
+        private UnitOfMeasurement unit;
+        private double numCalories;
+        private FoodGroup group;
+
+        public Ingredient(string ingredientName, double quantity, int unit, double numCalories, int group)
+        {
+            populateLists();
+            this.ingredientName = ingredientName;
+            this.quantity = quantity;
+            this.numCalories = numCalories;
+            this.unit = units.ElementAt(unit);
+            this.group = groups.ElementAt(group);
+
+        }// end constructor
+
+        public string display()
+        {
+            string display = "";
+            display = $"{quantity} {unit} of {ingredientName}\nFood Group: {group}\n{numCalories} calories";
+            return display;
         }
 
-        //constructor Method with 5 parameters
-        public Ingredient(string name, double quantity, UnitOfMeasurement unit, FoodGroup foodGroup, double calories)
-        {//constructor begin
-            Name = name;
-            Quantity = quantity;
-            Unit = unit;
-            OriginalQuantity = quantity;
-            OriginalUnit = unit; //needs to be stroed in an integer
-            FoodGroup = foodGroup;
-            this.calories = calories;
-            originalCalories = calories;
-        }//constructor end
+        //GETTERS
+        public string IngredientName()
+        { return ingredientName; }
+        public double Quantity()
+        { return quantity; }
+        public double NumCalories()
+        { return numCalories; }
+        public string Unit()
+        { return unit.ToString(); }
+        public string Group()
+        { return group.ToString(); }
 
-
-        //method to alter the Quatity to return to its orginal state
-        public void ResetQuantity()
-        {
-            Quantity = OriginalQuantity;
-        }
-        //method to alter the Unit to return to its orginal state
-        public void ResetUnit()
-        {
-            Unit = OriginalUnit;
-        }
-
-        //method to calulate the scaled cories (alter the calories using the scaling factor)
-        public void CalculateScaledCalories(double factor)
-        {
-            // Assuming CaloriesPerUnit is the number of calories per unit of the ingredient
-            calories = factor * calories;
-        }
-
-        //method to alter the calories to return to its orginal state
-        public void ResetCalories()
-        {
-            this.calories = originalCalories;
-        }
-
-        public void display()
-        { Console.WriteLine($"{Quantity} {Unit} of {Name}\nFood Group: {FoodGroup}\n{calories} calories\n"); }
-        // display method created to display the quantity along with the unit of measurment for each ingredient
-        // as well as number of calories and food group
-        // string interpolation used in display
-
-    }//Ingredient Class end
+    }// end ingredient class
 }//namespace end
